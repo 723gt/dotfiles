9cfb23abd046dae98b1b69f53060c791c243ce28
723 <rb.natsumi@gmail.com>
2018-05-14 21:57:14 +0900

    dotfileを編集

--- dotfileslinc.sh
+++ dotfileslinc.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.tmux_conf ~/.tmux_conf
+ln -sf ~/dotfiles/vim/plugin/ruby-matchit/plugin ~/.vim/
